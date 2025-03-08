e3f620a0789c98892ae6ec20ada02c9e5812330f
Eduardo2233073297 <166538616+Eduardo2233073297@users.noreply.github.com>
2025-03-07 19:22:17 -0600

    Add files via upload

---
+++ lenguajes digitales/script.js
@@ -0,0 +1,9 @@
+let nodo1 = "En mi opinión, las tecnologías que usamos para crear páginas web como HTML, CSS y JavaScript son importantes para mi desarrollo y evolución como artista en el campo digital, debido a que gracias a ellas me permite estructurar mis ideas de manera ordenada, creando el esqueleto de mis proyectos digitales. Gracias a su sencillez y flexibilidad, además puedo dar vida a mis conceptos artísticos de una manera clara, accesible y de formas más creativas e interactivas para el público en general, lo cual me permite expresarme de formas que antes no se tenían pensadas y acercarnos de mejor manera al espectador. Estas herramientas me brindan la posibilidad de combinar la estética visual con la interacción, lo que abre un abanico infinito de posibilidades para innovar y conectar con la audiencia de una forma más directa y personalizada."
+
+let nodo2 = "Además con estas tecnologías me ayudan a adentrarme  a nuevos campos y opciones para hacer obras artísticas, como lo puede ser el diseño visual asegurando que mi trabajo sea accesible desde cualquier pantalla y brindando una experiencia fluida y atractiva, Lo único malo que le puedo ver a este tipo de tecnologías son las limitantes que aún tienen y un poco su complejidad el cual puede llegar a limitar el proceso y el aprendizaje para las demás personas que de igual forma quieren hacer este tipo de obras o entornos web."
+
+document.getElementById('nodo1').innerHTML=nodo1;
+document.getElementById('nodo2').innerHTML=nodo2;
+
+console.log(nodo1.length);
+console.log(nodo2.length);
