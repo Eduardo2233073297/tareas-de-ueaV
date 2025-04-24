730804449e765794166162d8a1db5049322954cf
Eduardo2233073297 <166538616+Eduardo2233073297@users.noreply.github.com>
2025-04-23 20:51:54 -0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("Dibujando mi vida");
-let p1 ="El dibujo ha sido una parte fundamental de mi vida, más que una simple actividad creativa. Es mi forma de expresar mi creatividad. A través del dibujo, encuentro un espacio donde puedo conectar conmigo mismo y dar forma a mis ideas. Desde pequeño, ha sido una vía para explorar el mundo que me rodea y transformarlo según mi imaginación. Dibujar me ayuda a concentrarme, a relajarme y a descubrir nuevas perspectivas. Es una herramienta que me ha acompañado en momentos de alegría y también en tiempos difíciles, funcionando como una especie de refugio personal. En resumen, el dibujo no solo influye en mi vida: le da color, sentido y creatividad.";
-
-
-document.getElementById('parr1').innerHTML="parrafo 1".innerHTML=p1;
